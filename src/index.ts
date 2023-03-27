53c6620f78f8917c1ea350561a6e331ef1dd6dd8
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-03-27 10:31:57 +0100

    First commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+const app = express()
+app.use(express.json())
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`Sever running on Port ${PORT}`)
+    
+})

5e65e0f763b2c0b830119feb51bf0348aa911d24
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-03-30 12:01:32 +0100

    PokemonIntegration test(red): /Pokemon should give a list of pokemon

---
+++ tests/PokemonIntegration/PokemonIntegration.test.ts
@@ -0,0 +1,17 @@
+import {server} from "../../src";
+
+const request = require("supertest")
+
+afterEach(() => {
+    server.close()
+})
+
+describe("/Pokemon should", () => {
+    it("give a list of pokemon", async () => {
+        const response = await request(server)
+            .get("/api/pokemon")
+            .set('Accept', 'application/json');
+
+        expect(response.body.pokemonList.length).toBe(20)
+    })
+})
